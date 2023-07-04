88aed77a94e754a8c940a1eb1f1d835261b7b96a
Paulo Teixeira <pteixeira.dev@gmail.com>
2023-07-03 22:53:31 -0300

    Melhoria de estrutura de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,134 @@
-import React, { useState } from "react";
-import Timeline from "react-calendar-timeline";
-import "react-calendar-timeline/lib/Timeline.css";
 import { add, getTime } from "date-fns";
-import { Tooltip } from "@mui/material";
-import Button from "@mui/material/Button";
-import "./App.css";
-import moment from "moment";
-import "moment/locale/pt-br";
+import React, { useState } from "react";
 import CustomCalendarTimeline from "./components/CustomCalendarTimeline";
 
 function App() {
+  var keys = {
+    groupIdKey: "id",
+    groupTitleKey: "title",
+    groupRightTitleKey: "rightTitle",
+    itemIdKey: "id",
+    itemTitleKey: "title",
+    itemDivTitleKey: "title",
+    itemGroupKey: "group",
+    itemTimeStartKey: "start_time",
+    itemTimeEndKey: "end_time",
+    groupLabelKey: "title",
+  };
+
+  const [groups] = useState([
+    { id: 1, title: "group 1" },
+    { id: 2, title: "group 2" },
+    { id: 3, title: "group 3" },
+    { id: 4, title: "group 4" },
+    { id: 5, title: "group 5" },
+    { id: 6, title: "group 6" },
+    { id: 7, title: "group 7" },
+    { id: 8, title: "group 8" },
+    { id: 9, title: "group 9" },
+    { id: 10, title: "group 10" },
+    { id: 11, title: "group 11" },
+    { id: 12, title: "group 12" },
+    { id: 13, title: "group 13" },
+    { id: 14, title: "group 14" },
+    { id: 15, title: "group 15" },
+    { id: 16, title: "group 16" },
+    { id: 17, title: "group 17" },
+    { id: 18, title: "group 18" },
+    { id: 19, title: "group 19" },
+    { id: 20, title: "group 20" },
+    { id: 21, title: "group 21" },
+    { id: 22, title: "group 22" },
+  ]);
+
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      group: 1,
+      title: "ITEM N° 1",
+      start_time: getTime(new Date()),
+      end_time: getTime(add(new Date(), { hours: 1 })),
+      canMove: true,
+      canResize: "both",
+      className: "",
+      bgColor: "#1F9E27",
+      selectedBgColor: "rgba(244, 215, 151, 1)",
+      color: "white",
+    },
+    {
+      id: 2,
+      group: 10,
+      title: "ITEM N° 2",
+      start_time: getTime(add(new Date(), { hours: -5 })),
+      end_time: getTime(add(new Date(), { hours: 1 })),
+      canMove: true,
+      canResize: "both",
+      className: "",
+      bgColor: "#0065BB",
+      selectedBgColor: "#71CBD0",
+      color: "white",
+    },
+    {
+      id: 3,
+      group: 1,
+      title: "ITEM N° 3",
+      start_time: getTime(add(new Date(), { hours: -1 })),
+      end_time: getTime(add(new Date(), { hours: 3 })),
+      canMove: true,
+      canResize: "both",
+      className: "",
+      bgColor: "#B71C1C",
+      selectedBgColor: "rgba(244, 215, 151, 1)",
+      color: "white",
+    },
+  ]);
+
+  const onItemMove = (
+    itemId: number,
+    dragTime: number,
+    newGroupOrder: number
+  ) => {
+    const group = groups[newGroupOrder];
+
+    setItems(
+      items.map((item) =>
+        item.id === itemId
+          ? Object.assign({}, item, {
+              start_time: dragTime,
+              end_time: dragTime + (item.end_time - item.start_time),
+              group: group.id,
+            })
+          : item
+      )
+    );
+  };
+
+  const onItemResize = (
+    itemId: number,
+    time: number,
+    edge: "right" | "left"
+  ) => {
+    setItems(
+      items.map((item) =>
+        item.id === itemId
+          ? Object.assign({}, item, {
+              start_time: edge === "left" ? time : item.start_time,
+              end_time: edge === "left" ? item.end_time : time,
+            })
+          : item
+      )
+    );
+  };
+
   return (
     <div>
-      {/* <Timeline
-        groups={groups}
-        items={items as any}
+      <CustomCalendarTimeline
         keys={keys}
-        itemTouchSendsClick={false}
-        stackItems
-        itemHeightRatio={0.75}
-        canMove={false}
-        canResize={false}
-        lineHeight={40}
-        defaultTimeStart={add(new Date(), { hours: -12 })}
-        defaultTimeEnd={add(new Date(), { hours: 12 })}
-        onTimeChange={(st, end, update) => {
-          console.log("time change ============");
-          console.log(new Date(st));
-          console.log(new Date(end));
-          // debouncer
-          // função que verifica se esta buscando mais itens da esquerda ou direita
-          // disparar busca por dados de um mês frente ou atras
-          update(st, end);
-        }}
-        itemRenderer={itemRenderer}
-        onItemMove={handleItemMove}
-        onItemResize={handleItemResize}
-      /> */}
-      <CustomCalendarTimeline />
+        groups={groups}
+        items={items}
+        onItemMove={onItemMove}
+        onItemResize={onItemResize}
+      />
     </div>
   );
 }
--- src/components/CustomCalendarTimeline/index.tsx
+++ src/components/CustomCalendarTimeline/index.tsx
@@ -1,104 +1,65 @@
-import React, { useState } from "react";
-import Timeline from "react-calendar-timeline";
-import "react-calendar-timeline/lib/Timeline.css";
-import { add, getTime } from "date-fns";
-import { Tooltip } from "@mui/material";
+import React from "react";
+import Timeline, { Unit } from "react-calendar-timeline";
+import { add } from "date-fns";
+import { Tooltip, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
-import "../../App.css";
 import moment from "moment";
+import "./App.css";
+import "react-calendar-timeline/lib/Timeline.css";
 import "moment/locale/pt-br";
 
-const CustomCalendarTimeline = () => {
+interface IKeys {
+  groupIdKey: string;
+  groupTitleKey: string;
+  groupRightTitleKey: string;
+  itemIdKey: string;
+  itemTitleKey: string;
+  itemDivTitleKey: string;
+  itemGroupKey: string;
+  itemTimeStartKey: string;
+  itemTimeEndKey: string;
+  groupLabelKey: string;
+}
+
+interface IGroup {
+  id: number;
+  title: string;
+}
+
+interface IItem {
+  id: number;
+  group: number;
+  title: string;
+  start_time: number;
+  end_time: number;
+  canMove: boolean;
+  canResize: string;
+  className: string;
+  bgColor: string;
+  selectedBgColor: string;
+  color: string;
+}
+
+interface CustomCalendarTimelineProps {
+  keys: IKeys;
+  groups: IGroup[];
+  items: IItem[];
+  onItemMove: (itemId: number, dragTime: number, newGroupOrder: number) => void;
+  onItemResize: (itemId: number, time: number, edge: "right" | "left") => void;
+}
+
+const CustomCalendarTimeline: React.FC<CustomCalendarTimelineProps> = ({
+  keys,
+  groups,
+  items,
+  onItemMove,
+  onItemResize,
+}) => {
   moment.locale("pt-br");
-  var keys = {
-    groupIdKey: "id",
-    groupTitleKey: "title",
-    groupRightTitleKey: "rightTitle",
-    itemIdKey: "id",
-    itemTitleKey: "title",
-    itemDivTitleKey: "title",
-    itemGroupKey: "group",
-    itemTimeStartKey: "start_time",
-    itemTimeEndKey: "end_time",
-    groupLabelKey: "title",
-  };
-
-  const [groups] = useState([
-    { id: 1, title: "group 1" },
-    { id: 2, title: "group 2" },
-    { id: 3, title: "group 3" },
-    { id: 4, title: "group 4" },
-    { id: 5, title: "group 5" },
-    { id: 6, title: "group 6" },
-    { id: 7, title: "group 7" },
-    { id: 8, title: "group 8" },
-    { id: 9, title: "group 9" },
-    { id: 10, title: "group 10" },
-    { id: 11, title: "group 11" },
-    { id: 12, title: "group 12" },
-    { id: 13, title: "group 13" },
-    { id: 14, title: "group 14" },
-    { id: 15, title: "group 15" },
-    { id: 16, title: "group 16" },
-    { id: 17, title: "group 17" },
-    { id: 18, title: "group 18" },
-    { id: 19, title: "group 19" },
-    { id: 20, title: "group 20" },
-    { id: 21, title: "group 21" },
-    { id: 22, title: "group 22" },
-    { id: 23, title: "group 23" },
-    { id: 24, title: "group 24" },
-    { id: 25, title: "group 25" },
-    { id: 26, title: "group 26" },
-    { id: 27, title: "group 27" },
-    { id: 28, title: "group 28" },
-    { id: 29, title: "group 29" },
-    { id: 30, title: "group 30" },
-    { id: 31, title: "group 31" },
-    { id: 32, title: "group 32" },
-  ]);
-
-  const [items] = useState([
-    {
-      id: 1,
-      group: 1,
-      title: "item 1",
-      start_time: getTime(new Date()),
-      end_time: getTime(add(new Date(), { hours: 1 })),
-      canMove: true,
-      canResize: "both",
-      className: "",
-      bgColor: "rgba(244, 215, 151, 0.6)",
-      selectedBgColor: "rgba(244, 215, 151, 1)",
-      color: "#c48909",
-    },
-    {
-      id: 2,
-      group: 10,
-      title: "item 2",
-      start_time: getTime(add(new Date(), { hours: -5 })),
-      end_time: getTime(add(new Date(), { hours: 1 })),
-      canMove: true,
-      canResize: "both",
-      className: "",
-      bgColor: "blue",
-      selectedBgColor: "blueviolet",
-      color: "#c48909",
-    },
-    {
-      id: 3,
-      group: 28,
-      title: "item 3",
-      start_time: getTime(add(new Date(), { hours: -1 })),
-      end_time: getTime(add(new Date(), { hours: 3 })),
-      canMove: true,
-      canResize: "both",
-      className: "",
-      bgColor: "rgba(244, 215, 151, 0.6)",
-      selectedBgColor: "rgba(244, 215, 151, 1)",
-      color: "#c48909",
-    },
-  ]);
+  const DEFAULT_TIME_START = add(new Date(), { hours: -12 });
+  const DEFAULT_TIME_END = add(new Date(), { hours: 12 });
+  const DRAGGING_BG_COLOR = "red";
+  const RESIZING_BORDER_COLOR = "red";
 
   const itemRenderer = ({
     item,
@@ -110,36 +71,41 @@ const CustomCalendarTimeline = () => {
     const { left: leftResizeProps, right: rightResizeProps } = getResizeProps();
     const backgroundColor = itemContext.selected
       ? itemContext.dragging
-        ? "red"
+        ? DRAGGING_BG_COLOR
         : item.selectedBgColor
       : item.bgColor;
-    const borderColor = itemContext.resizing ? "red" : item.color;
+    const borderColor = itemContext.resizing
+      ? RESIZING_BORDER_COLOR
+      : item.bgColor;
+    const wrapperProps = getItemProps({
+      style: {
+        backgroundColor,
+        color: item.color,
+        borderColor,
+        borderStyle: "solid",
+        borderWidth: 1,
+        borderRadius: 4,
+        borderLeftWidth: itemContext.selected ? 6 : 1,
+        borderRightWidth: itemContext.selected ? 6 : 1,
+      },
+    });
+    console.log("PROPS");
+    console.log(wrapperProps);
+
     return (
-      <div
-        {...getItemProps({
-          style: {
-            backgroundColor,
-            color: item.color,
-            borderColor,
-            borderStyle: "solid",
-            borderWidth: 1,
-            borderRadius: 4,
-            borderLeftWidth: itemContext.selected ? 6 : 1,
-            borderRightWidth: itemContext.selected ? 6 : 1,
-          },
-        })}
-      >
+      <div {...wrapperProps}>
         {itemContext.useResizeHandle ? <div {...leftResizeProps} /> : null}
-        <div className="table-custom-item-wrapper">
+        <div
+          className={itemContext.selected ? "table-custom-item-wrapper" : ""}
+        >
           <Tooltip
             arrow
-            style={{ maxWidth: "none !important" }}
             title={
               <div
                 style={{
                   height: "100px",
-                  width: "400px",
-                  backgroundColor: "red",
+                  width: "300px",
+                  // backgroundColor: "red",
                 }}
               >
                 CUSTOM TOOLTIP
@@ -166,64 +132,58 @@ const CustomCalendarTimeline = () => {
     );
   };
 
-  const handleItemMove = (itemId: any, dragTime: any, newGroupOrder: any) => {
-    const group = groups[newGroupOrder];
-
-    console.log({
-      items: items.map((item) =>
-        item.id === itemId
-          ? Object.assign({}, item, {
-              start_time: dragTime,
-              end_time: dragTime + (item.end_time - item.start_time),
-              group: group.id,
-            })
-          : item
-      ),
-    });
-
-    console.log("Moved", itemId, dragTime, newGroupOrder);
+  const handleItemMove = (
+    itemId: number,
+    dragTime: number,
+    newGroupOrder: number
+  ) => {
+    onItemMove(itemId, dragTime, newGroupOrder);
   };
 
-  const handleItemResize = (itemId: any, time: any, edge: any) => {
-    console.log({
-      items: items.map((item) =>
-        item.id === itemId
-          ? Object.assign({}, item, {
-              start_time: edge === "left" ? time : item.start_time,
-              end_time: edge === "left" ? item.end_time : time,
-            })
-          : item
-      ),
-    });
+  const handleItemResize = (
+    itemId: number,
+    time: number,
+    edge: "right" | "left"
+  ) => {
+    onItemResize(itemId, time, edge);
+  };
+
+  const handleTimeChange = (
+    visibleTimeStart: number,
+    visibleTimeEnd: number,
+    updateScrollCanvas: (start: number, end: number) => void,
+    unit: Unit
+  ) => {
+    console.log("time change ============");
+    console.log(new Date(visibleTimeStart));
+    console.log(new Date(visibleTimeEnd));
+    // debouncer
+    // função que verifica se esta buscando mais itens da esquerda ou direita
+    // disparar busca por dados de um mês frente ou atras
+    updateScrollCanvas(visibleTimeStart, visibleTimeEnd);
   };
 
   return (
-    <div>
-      <Timeline
-        groups={groups}
-        items={items as any}
-        keys={keys}
-        itemTouchSendsClick={false}
-        stackItems
-        itemHeightRatio={0.75}
-        canMove={false}
-        canResize={false}
-        lineHeight={40}
-        defaultTimeStart={add(new Date(), { hours: -12 })}
-        defaultTimeEnd={add(new Date(), { hours: 12 })}
-        onTimeChange={(st, end, update) => {
-          console.log("time change ============");
-          console.log(new Date(st));
-          console.log(new Date(end));
-          // debouncer
-          // função que verifica se esta buscando mais itens da esquerda ou direita
-          // disparar busca por dados de um mês frente ou atras
-          update(st, end);
-        }}
-        itemRenderer={itemRenderer}
-        onItemMove={handleItemMove}
-        onItemResize={handleItemResize}
-      />
+    <div className="custom-calendar-timeline-wrapper">
+      <Typography>
+        <Timeline
+          groups={groups}
+          items={items as any}
+          keys={keys}
+          itemTouchSendsClick={false}
+          stackItems
+          itemHeightRatio={0.75}
+          canMove={false}
+          canResize={false}
+          lineHeight={40}
+          defaultTimeStart={DEFAULT_TIME_START}
+          defaultTimeEnd={DEFAULT_TIME_END}
+          onTimeChange={handleTimeChange}
+          itemRenderer={itemRenderer}
+          onItemMove={handleItemMove}
+          onItemResize={handleItemResize}
+        />
+      </Typography>
     </div>
   );
 };
